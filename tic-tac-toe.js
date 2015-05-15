d984a8eedf6c76a9cf97e30c4fcc1b8b10022e46
Zach Fountain <zach.fountain@gmail.com>
2015-05-15 10:44:01 -0500

    Added click count to js; fixed reset color; styled New Game button.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,3 @@
-// Define tic-tac-toe board layout.
-var board = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']];
-
 // Player clicks a div/spot on the board.
 // Function determines whether player is X or O and writes the text to the selected/clicked div.
 var currentPlayer = "X"
@@ -12,15 +9,15 @@ $(".column-four").click(function() {
 			(currentPlayer = "X");
 			checkBoard();
 			findWinner();
+			onClick();
 		}
 		else if (currentPlayer == "X") {
 			(currentPlayer = "O");
 			checkBoard();
 			findWinner();
+			onClick();
 		}
 });
-
-// Play continues until one player wins by getting (a,b,c), (d,e,f), (g,h,i), (a,d,g), (b,e,h), (c,f,i), (a,e,i) or (c,e,g) OR until the game is tied with all 9 spots on the board filled.
 
 // Declare variables for each square.
 var a;
@@ -46,6 +43,16 @@ checkBoard = function() {
 	i = $("#i").text();
 };
 
+// Play continues until one player wins by getting (a,b,c), (d,e,f), (g,h,i), (a,d,g), (b,e,h), (c,f,i), (a,e,i) or (c,e,g) OR until the game is tied with all 9 spots on the board filled.
+
+var clickCount = 0;
+	function onClick() {
+	clickCount += 1;
+	document.getElementsByClassName("column-four").innerHTML = clickCount;
+};
+ 
+
+
 findWinner = function() {
 // Check to see if X won
 	if ((a === "X" && b === "X" && c === "X") || (d === "X" && e === "X" && f === "X") || (g === "X" && h === "X" && i === "X") || (a === "X" && d === "X" && g === "X") || (b === "X" && e === "X" && h === "X") || (c === "X" && f === "X" && i === "X") || (a === "X" && e === "X" && i === "X") || (c === "X" && e === "X" && g === "X")) {
@@ -60,16 +67,16 @@ findWinner = function() {
 
 // Function will clear the board.
 // TO DO: Add functionality to reset column color
-var newGame;
 var resetBoard = $("#reset").click(function(event) {
-	a = $("#a").text("");
-	b = $("#b").text("");
-	c = $("#c").text("");
-	d = $("#d").text("");
-	e = $("#e").text("");
-	f = $("#f").text("");
-	g = $("#g").text("");
-	h = $("#h").text("");
-	i = $("#i").text("");
-	newGame();
+	a = $("#a").text("").css("background-color", "#F2E4AA");
+	b = $("#b").text("").css("background-color", "#F2E4AA");
+	c = $("#c").text("").css("background-color", "#F2E4AA");
+	d = $("#d").text("").css("background-color", "#F2E4AA");
+	e = $("#e").text("").css("background-color", "#F2E4AA");
+	f = $("#f").text("").css("background-color", "#F2E4AA");
+	g = $("#g").text("").css("background-color", "#F2E4AA");
+	h = $("#h").text("").css("background-color", "#F2E4AA");
+	i = $("#i").text("").css("background-color", "#F2E4AA");
+	currentPlayer = "X";
+	clickCount = 0;
 });
